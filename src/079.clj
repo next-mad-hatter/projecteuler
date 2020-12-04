a8ba4290c4e09fa19b59fe82cd98db6c07396ba1
Max Deineko <max.deineko@gmail.com>
2020-12-04 16:56:09 +0100

    Solved problem 79

---
+++ src/079.clj
@@ -0,0 +1,38 @@
+(ns madhat.adventofcode.day-three
+  (:require [clojure.string :as str]))
+
+(defn input->words [input]
+  (as-> input $
+    (str/split $ #"\R+")
+    (map seq $)
+    (into #{} $)))
+
+(def words
+  (->>
+   "079_keylog.txt"
+   (slurp)
+   (input->words)))
+
+(def shortest
+
+  (letfn [(shorten-word [ch word]
+            (if (= ch (first word)) (next word) word))
+          (shorten-words [ch words]
+            (->> words
+                 (map (partial shorten-word ch))
+                 (remove nil?)
+                 (into #{})))]
+    (memoize
+     (fn [words]
+       (if (empty? words)
+         ""
+         (let [prefixes     (distinct (map first words))
+               suffixes     (map #(shorten-words % words) prefixes)
+               subsolutions (map shortest suffixes)
+               solutions    (map cons prefixes subsolutions)]
+           (apply min-key count solutions)))))))
+
+(time
+ (->> words
+      shortest
+      (apply str)))
